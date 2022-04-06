7e57ea5568eb37f9a28dc8432a952d8f81a077fd
ninaschramm <aline.carb@gmail.com>
2022-04-06 17:14:48 -0300

    função dealCards()

--- scripts.js
+++ scripts.js
@@ -1,1 +1,40 @@
-let cardsNumber = prompt("Com quantas cartas você deseja jogar?")
+let cardsNumber
+
+do { 
+   cardsNumber = prompt("Com quantas cartas você deseja jogar?");
+} while (cardsNumber % 2 != 0 || cardsNumber > 14 || cardsNumber < 4);
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+let cardList = []
+
+function dealCards() {
+   for (i=0; i<cardsNumber/2; i++) {
+      cardList.push(`<div class='card parrot${i}' onclick='turnCard(this)'></div>`)
+      cardList.push(`<div class='card parrot${i}' onclick='turnCard(this)'></div>`)
+   }
+console.log(cardList)
+cardList.sort(comparador)
+
+   for (j=0; j<cardList.length; j++) { 
+      document.querySelector(".cards").innerHTML += cardList[j]
+   }
+}
+
+
+
+function countMoves() {
+    
+}
+
+dealCards()
+
+function turnCard(elt) {
+   elt.classList.add("turned")
+}
+
+function compareCard() {
+   
+}
