d730f394dafe49bad7182ece9c6654d1a47f3b26
ninaschramm <aline.carb@gmail.com>
2022-04-08 18:37:14 -0300

    layout responsivo, turnCards()

--- scripts.js
+++ scripts.js
@@ -8,12 +8,24 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
+const parrotList = [
+   {name: "parrot1", img: 'arq/fiestaparrot.gif', status: 'notselected'},
+   {name: "parrot2", img: 'arq/bobrossparrot.gif', status: 'notselected'},
+   {name: "parrot3", img: 'arq/metalparrot.gif', status: 'notselected'},
+   {name: "parrot4", img: 'arq/explodyparrot.gif', status: 'notselected'},
+   {name: "parrot5", img: 'arq/unicornparrot.gif', status: 'notselected'},
+   {name: "parrot6", img: 'arq/tripletsparrot.gif', status: 'notselected'},
+   {name: "parrot7", img: 'arq/revertitparrot.gif', status: 'notselected'}
+]
+
 let cardList = []
 
 function dealCards() {
+   
    for (i=0; i<cardsNumber/2; i++) {
-      cardList.push(`<div class='card parrot${i}' onclick='turnCard(this)'></div>`)
-      cardList.push(`<div class='card parrot${i}' onclick='turnCard(this)'></div>`)
+      const parrot = parrotList[i];
+      cardList.push(`<div class='card ${parrot.name}' onclick='turnCard(this)'><img src='${parrot.img}'></div>`)
+      cardList.push(`<div class='card ${parrot.name}' onclick='turnCard(this)'><img src='${parrot.img}'></div>`)
    }
 console.log(cardList)
 cardList.sort(comparador)
@@ -24,7 +36,6 @@ cardList.sort(comparador)
 }
 
 
-
 function countMoves() {
     
 }
@@ -33,8 +44,38 @@ dealCards()
 
 function turnCard(elt) {
    elt.classList.add("turned")
+   const statusList = []
+   for (let i=0; i<parrotList.length; i++) {
+      statusList.push(parrotList[i].status)}
+
+   if (statusList.includes("selected")) {
+         compareCard(elt)
+      }
+
+   else {
+   for (let i=0; i<parrotList.length; i++) {
+      if (elt.classList.contains(`${parrotList[i].name}`) && parrotList[i].status == "notselected") {
+         parrotList[i].status = "selected"
+      }
+   }
 }
 
-function compareCard() {
-   
+// unturnCards()
+
 }
+
+function compareCard(elt) {
+   if (elt.classList.contains(`${parrotList[i].name}`) && parrotList[i].status == "selected") {
+      parrotList[i].status = "matched"
+   }
+   else {parrotList[i].status = "notselected"}
+}
+
+function unturnCards() {
+   let cardsTurned = document.querySelectorAll(".turned");
+   for (let h=0; h<cardsTurned.length; h++) {
+      cardsTurned[h].classList.remove('turned')
+      }
+   }
+   
+
